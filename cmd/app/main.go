34da3a7832b60c8e2faddbf14740c1eb77ad11ff
Jakub Zilinek <zilinek.jakub@gmail.com>
2025-05-23 15:06:58 +0200

    implemented context to controller to allow easier testing

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,16 +6,31 @@ import (
 	"GoHealthChecker/internal/service"
 	"GoHealthChecker/internal/store"
 	"GoHealthChecker/internal/view"
+	"context"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	TIMEOUT := 10
+	// Create a cancellable context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Set up signal handling
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+
+	// Handle signals in a separate goroutine
+	go func() {
+		<-signalCh
+		cancel() // Cancel context on Ctrl+C
+	}()
 
 	inMemoryStore := store.NewInMemoryStore()
 	CLIView := view.NewCLIView()
 	HTTPService := service.NewHTTPService(TIMEOUT)
 
-	appController := controller.NewController(inMemoryStore, CLIView, HTTPService)
+	appController := controller.NewController(inMemoryStore, CLIView, HTTPService, ctx)
 	internal.LOGGER.Info("Starting the app.")
 	appController.Start()
 }
